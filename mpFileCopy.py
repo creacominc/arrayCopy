7f2b15eca0d79ca08d9e351d0e92cc50beb0820c
creacominc <haroldt@creacom.ca>
2020-02-22 01:16:09 -0500

    Added process-specific logging, dry-run and move options.

--- mpFileCopy.py
+++ mpFileCopy.py
@@ -14,11 +14,11 @@ from datetime import datetime
 from enum import Enum, auto
 import multiprocessing
 from multiprocessing import Pool
-from logging.handlers import QueueHandler, QueueListener
+from logging.handlers import QueueHandler, QueueListener, RotatingFileHandler
 
 class ERR(Enum):
-    SOURCE_DOES_NOT_EXIST = auto()
-    TARGET_DOES_NOT_EXIST = auto()
+    SOURCE_PATH_DOES_NOT_EXIST = auto()
+    TARGET_PATH_DOES_NOT_EXIST = auto()
     SOURCE_TARGET_MISMATCH = auto()
 
 
@@ -28,16 +28,22 @@ class LeafFinder:
     m_sourcePath = ""
     m_targetPath = ""
     m_logLevel   = logging.INFO
+    m_dry = False
+    m_move = False
 
-    def __init__( self, src, trg, thrds, level ):
+
+    def __init__( self, src, trg, thrds, dry, move, level ):
         self.m_threads = int(thrds)
         self.m_sourcePath = src
         self.m_targetPath = trg
         self.m_nodes = []
+        self.m_dry = dry
+        self.m_move = move
         self.m_logLevel = level
 
     def confirmFolderExists( self, path ) -> bool:
         """ return true if the path is a folder that exists """
+        logger = logging.getLogger()
         if not os.path.exists( path ):
             logger.error( f"Folder ({path}) does not exist." )
             return False
@@ -50,6 +56,7 @@ class LeafFinder:
         return ( os.path.basename( os.path.realpath(src) ) == os.path.basename( os.path.realpath(trg) ) )
 
     def findAllLeafNodes( self, currentPath ):
+        logger = logging.getLogger()
         # if the path is a folder, iterate of the contents can call this function recursively
         fullCurrent = os.path.join( self.m_sourcePath, currentPath )
         if os.path.isdir( fullCurrent ):
@@ -61,6 +68,7 @@ class LeafFinder:
         return
 
     def rsyncFile( self, filePath ):
+        logger = logging.getLogger()
         src = os.path.join( self.m_sourcePath, filePath )
         trg = os.path.dirname( os.path.join( self.m_targetPath, filePath ) )
         logger.debug( f"Copying src={src}\n\ttrg={trg}" )
@@ -69,7 +77,20 @@ class LeafFinder:
             os.makedirs( trg, mode=0o775, exist_ok=True )
         # -e ssh -T -c arcfour -o Compression=no
         #  --progress
-        cmdpre = 'rsync   -v -v --progress  --perms --links --times --itemize-changes --stats --backup --suffix=.backup  --exclude=.DS_Store --exclude=.Trashes --exclude=.Trash --exclude=._.Trashes --exclude=.localized --exclude=.DocumentRevisions-* --exclude=.Spotlight* --exclude=.fseventsd --exclude=.apdisk --exclude=.com.apple.timemachine.donotpresent --exclude=.fcplock --exclude=.fcpuser --exclude=.fseventsd --exclude=.cache --exclude=._.TemporaryItems --exclude=._.apdisk --exclude=.TemporaryItems'
+        cmdpre = 'rsync   -v -v --progress  --perms --links --times --itemize-changes --stats'
+        cmdpre += ' --backup --suffix=.backup  --exclude=.DS_Store --exclude=.Trashes --exclude=.Trash'
+        cmdpre += ' --exclude=._.Trashes --exclude=.localized --exclude=.DocumentRevisions-*'
+        cmdpre += ' --exclude=.Spotlight* --exclude=.fseventsd --exclude=.apdisk'
+        cmdpre += ' --exclude=.com.apple.timemachine.donotpresent --exclude=.fcplock --exclude=.fcpuser'
+        cmdpre += ' --exclude=.fseventsd --exclude=.cache --exclude=._.TemporaryItems --exclude=._.apdisk'
+        cmdpre += ' --exclude=.TemporaryItems'
+        # add dry-run if specified
+        if self.m_dry:
+            cmdpre += ' --dry-run '
+        # add move if specified
+        if self.m_move:
+            cmdpre += ' --remove-source-files '
+        #
         cmdfilt0='--filter=dir-merge /.rsync.include'
         cmdfilt1='--filter=dir-merge /.rsync.exclude'
         cmd = cmdpre + ' ' + cmdfilt0 + ' ' + cmdfilt1 + ' "' + src + '" "' + trg + '"'
@@ -77,7 +98,6 @@ class LeafFinder:
         print( f' ========== {cmd}' )
         cmdArr = cmdpre.split()
         cmdArr += [ cmdfilt0, cmdfilt1, src, trg ]
-        #res = subprocess.run( cmdArr, capture_output=True, text=True )
         res = subprocess.run( cmdArr, capture_output=True, text=False )
         if( res.returncode != 0 ):
             logging.error( res )
@@ -87,24 +107,36 @@ class LeafFinder:
         for line in res.stdout.decode("latin-1").split('\n'):
             print( line )
             logging.info( line )
+        return( src )
 
+    def worker_init(self):
+        pidName = f'mpFileCopy.{ os.getpid() }.log'
+        logging.basicConfig( filename=pidName, level=self.m_logLevel )
+        logger = logging.getLogger()
+        logger.setLevel( self.m_logLevel )
+        logging.info( f'================== { datetime.now() }')
 
     def mpRsyncCopy( self ):
         ''' create m_threads processes of files and do rsyncCopy on each. '''
-        with Pool( processes=self.m_threads ) as pool:
-            pool.map( self.rsyncFile, self.m_nodes )
+        print( f'Threads == {self.m_threads}' )
+        pool = multiprocessing.Pool( self.m_threads, self.worker_init )
+        for result in pool.map( self.rsyncFile, self.m_nodes ):
+            logging.info( f'Process completed: {result}' )
+        pool.close()
+        pool.join()
 
     def run( self ) -> int:
         """Verify that the source and target exist and are the same folder name, then find the leaf nodes of the source."""
+        logger = logging.getLogger()
         logger.info( f" ========================================  start: {datetime.now()}" )
         if not self.confirmFolderExists( self.m_sourcePath ):
             logger.error( f"Source path does not exist: {self.m_sourcePath}" )
             logger.info( f" ========================================    end: {datetime.now()}" )
-            return ERR.SOURCE_DOES_NOT_EXIST
+            return ERR.SOURCE_PATH_DOES_NOT_EXIST
         if not self.confirmFolderExists( self.m_targetPath ):
             logger.error( f"Target path does not exist: {self.m_targetPath}" )
             logger.info( f" ========================================    end: {datetime.now()}" )
-            return ERR.TARGET_DOES_NOT_EXIST
+            return ERR.TARGET_PATH_DOES_NOT_EXIST
         if not self.leafNodesMatch( self.m_sourcePath, self.m_targetPath ):
             logger.error( f"Source and Target must have the same starting point." )
             logger.info( f"Source = {self.m_sourcePath}" )
@@ -120,18 +152,32 @@ class LeafFinder:
         logger.info( f" ========================================    end: {datetime.now()}" )
 
 
+
+def logger_init( logLevel ):
+    rfh = RotatingFileHandler( filename='mpFileCopy.control.log', mode='a', maxBytes=1024000000, backupCount=9 )
+    rfh.doRollover()
+    rfh.setFormatter(logging.Formatter("%(levelname)s: %(asctime)s - %(process)s - %(message)s"))
+    logger = logging.getLogger()
+    logger.setLevel( logLevel )
+    logger.addHandler( rfh )
+    logging.info( f' ==================== { datetime.now() }')
+
+
 if __name__ == '__main__':
-    logger = logging.getLogger("mpFileCopy.logger")
     parser = argparse.ArgumentParser( description='Copy files from the source folder to the target path on separate processes.' )
     parser.add_argument( "--source", "-s", metavar="srcPath", help="Source Path", required=True )
     parser.add_argument( "--target", "-t", metavar="trgPath", help="Target Path", required=True )
     parser.add_argument( "--threads", "-n", metavar="threads", help="Number of threads", default=1 )
+    parser.add_argument( "--dry", "-d", help="Perform a Dry Run", action='store_true', default=False, required=False )
+    parser.add_argument( "--move", "-m", help="Move files", action='store_true', default=False, required=False )
     parser.add_argument( "--log", "-l", dest="loglevel", metavar="loglevel", help="Log leval as WARN, INFO, DEBUG, etc", default="INFO" )
     args = parser.parse_args()
     numeric_level = getattr(logging, args.loglevel.upper(), None )
+    #
+    logger_init( numeric_level )
     if not isinstance( numeric_level, int ):
         raise ValueError( f"Invalid log level: {args.loglevel}" )
     logging.basicConfig( filename="mpFileCopy.log", level=numeric_level )
-    leafFinder = LeafFinder(  args.source, args.target, args.threads, numeric_level )
+    leafFinder = LeafFinder( args.source, args.target, args.threads, args.dry, args.move, numeric_level )
     sys.exit( leafFinder.run() )
 
